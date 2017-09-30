5c093471ad6ddb9e9622db1ab383f7784e603ca0
rchaser53 <tayoshizawa29@gmail.com>
2017-10-01 00:03:48 +0900

    try to create css-min-map

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const concat = new Concat(true, distFileName, '\n');
 
 const srcRoot = `${__dirname}/node_modules/formstone/dist/css/`;
 
-concat.add(`background.css`, fs.readFileSync(`${srcRoot}background.css`));
-concat.add(`carousel.css`, fs.readFileSync(`${srcRoot}carousel.css`));
+concat.add(`${srcRoot}background.css`, fs.readFileSync(`${srcRoot}background.css`));
+concat.add(`${srcRoot}carousel.css`, fs.readFileSync(`${srcRoot}carousel.css`));
  
 var concatenatedContent = concat.content;
 var sourceMapForContent = concat.sourceMap;
 
-fs.writeFileSync(distFileName, concatenatedContent);
+fs.writeFileSync(distFileName, `${concatenatedContent}
+/*# sourceMappingURL=formstone.min.css.map */
+`);
+fs.writeFileSync(`${distFileName}.map`, sourceMapForContent);
